255efa32ee970c2a8f1829a3278dc3eb24098e0c
Matt Fellows <matt.fellows@onegeek.com.au>
2022-12-20 14:12:40 +1100

    feat: support logging a panic

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/google/uuid"
 )
 
 func main() {
+	defer handlePanic()
+
 	initLogging()
 
 	port, err := GetFreePort()
@@ -20,4 +23,17 @@ func main() {
 		Port:      port,
 		ServerKey: uuid.NewString(),
 	})
+}
+
+func handlePanic() {
+	if r := recover(); r != nil {
+		l := len(fmt.Sprintf("recovered from panic: %v", r))
+		stars := make([]byte, l)
+		for i := 0; i < l; i++ {
+			stars[i] = '*'
+		}
+		log.Println("[ERROR]", string(stars))
+		log.Println("[ERROR] recovered from panic:", r)
+		log.Println("[ERROR]", string(stars))
+	}
 }
